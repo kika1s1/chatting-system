d756793a6ee88dfd149a85e34c948749b233e474
kika1s1 <tamiratkebede120@gmail.com>
2025-04-15 22:49:22 +0300

    Refactor project structure and implement authentication routes
    - Updated main entry point in package.json to server/src/index.js
    - Changed server start script to use nodemon for automatic restarts
    - Added new dependencies for authentication and database management
    - Removed old server/index.js file
    - Created new README.md with React + Vite setup instructions
    - Implemented database connection configuration in server/src/config/connectDB.js
    - Developed authentication controller with signup, login, and logout functions
    - Set up Express server in server/src/index.js and integrated authentication routes
    - Created authentication routes in server/src/routes/auth.route.js

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+export const signup =(req, res)=>{
+    console.log("signup route")
+    res.send("signup route")
+}
+
+
+export const logout = (req, res)=>{
+    res.send("logout route")
+}
+
+
+export const login = (req, res)=>{
+    console.log("login route")
+    res.send("login route")
+
+}
---
+++ server/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+// auth routes 
+app.use("/api/v1/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
